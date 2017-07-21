3b07e6f665bb479b8e36d45af3a97fb613a6cf9d
AQP20171679 <alejandraadriantejada@gmail.com>
2017-07-21 18:01:02 -0500

    Optimizando codigo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 var usuarios = [];
-var celdas = document.getElementsByTagName('td');
 var asientos = [];
+var celdas = document.getElementsByTagName('td');
+var _nombre = document.getElementById("nombre");
+var _apellido = document.getElementById("apellido");
+var _dni = document.getElementById("dni");
+
 for (var i = 0; i < celdas.length; i++) {
     celdas[i].addEventListener('click',redirect,false);
     asientos.push(celdas[i].textContent);
 }
-var _asiento=0;
-
 
 function redirect(event){
 	var _asiento = (event.target.textContent)
@@ -14,29 +16,28 @@ function redirect(event){
 	var mensaje=" esta desocupado"
 	
 	for (var i = 0; i < usuarios.length; i++) {
-		if(celdas[num].textContent==usuarios[i].n){
-    		mensaje=" esta ocupado"
-    	}
-    	
+		if(celdas[num].textContent==usuarios[i].n)
+    		mensaje=" esta ocupado"    	
 	}
+
 	if(mensaje==" esta ocupado"){
 		for(var i=0; i<usuarios.length; i++){
 			if(usuarios[i].n==_asiento){
-				document.getElementById("nombre").placeholder=usuarios[i].nombre;
-				document.getElementById("apellido").placeholder=usuarios[i].apellido;
-				document.getElementById("dni").placeholder=usuarios[i].dni;
+				_nombre.placeholder=usuarios[i].nombre;
+				_apellido.placeholder=usuarios[i].apellido;
+				_dni.placeholder=usuarios[i].dni;
 			}	
 		}
-    	document.getElementById("nombre").readOnly = true;
-		document.getElementById("apellido").readOnly = true;
-		document.getElementById("dni").readOnly = true;
+    	_nombre.readOnly = true;
+		_apellido.readOnly = true;
+		_dni.readOnly = true;
     }else{
-    	document.getElementById("nombre").placeholder="Nombre de Usuario";
-		document.getElementById("apellido").placeholder="Apellido de Usuario";
-		document.getElementById("dni").placeholder="DNI de Usuario";
-    	document.getElementById("nombre").readOnly = false;
-		document.getElementById("apellido").readOnly = false;
-		document.getElementById("dni").readOnly = false;
+    	_nombre.placeholder="Nombre de Usuario";
+		_apellido.placeholder="Apellido de Usuario";
+		_dni.placeholder="DNI de Usuario";
+    	_nombre.readOnly = false;
+		_apellido.readOnly = false;
+		_dni.readOnly = false;
     }
 
 	limpiar();
@@ -52,72 +53,77 @@ function reservar(){
 		nombre: nombre,
 		apellido:apellido,
 		dni: dni,
-		n:asiento}
+		n:asiento
+	}
 
-	var num = asientos.indexOf(asiento) 
-	celdas[num].style.backgroundColor="DodgerBlue";
-	document.getElementById("mostrar").innerHTML = "El asiento " + asiento + " esta ocupado";
+	if ((nombre == "")||(apellido == "")||(dni == ""))
+   		alert("Faltan ingresar datos");
+	else{
+  		var num = asientos.indexOf(asiento) 
+		celdas[num].style.backgroundColor = "DodgerBlue";
+		document.getElementById("mostrar").innerHTML = "El asiento " + asiento + " esta ocupado";
 
-	usuarios.push(persona);
+		usuarios.push(persona);
+		alert("Usuario registrado");
 
-	alert("Usuario registrado");
-
-	limpiar();
+		limpiar();
+  	}
 }
 
 function listar(){
 	var listaUsuarios="";
-	for(var i=0; i<usuarios.length; i++){
-		listaUsuarios+= "<h3><strong> Usuario </strong>"+ (i+1) +": <br></h3>"+
+	for( var i = 0 ; i < usuarios.length ; i++ ){
+		listaUsuarios += "<h3><strong> Usuario </strong>"+ (i+1) +": <br></h3>"+
 						"<strong>Nombre de Usuario: </strong>"+ usuarios[i].nombre +"<br>"+ 
 						"<strong>Apellido de Usuario: </strong>"+ usuarios[i].apellido+"<br>"+
 						"<strong>DNI de Usuario: </strong>"+ usuarios[i].dni+"<br>"+
 						"<strong>Asiento Ocupado: </strong>"+ usuarios[i].n+"<br>"
 	}
-	document.getElementById("lista").innerHTML= listaUsuarios;
+	document.getElementById("lista").innerHTML = listaUsuarios;
 }
 
-//{}
 function cancelar(){
-	var asiento= document.getElementById("mostrar").textContent.substr(11,2);
-	document.getElementById("nombre").placeholder="Nombre de Usuario";
-	document.getElementById("apellido").placeholder="Apellido de Usuario";
-	document.getElementById("dni").placeholder="DNI de Usuario";
-	var num = asientos.indexOf(asiento) 
-	celdas[num].style.backgroundColor="White";
+	var asiento = document.getElementById("mostrar").textContent.substr(11,2);
+	_nombre.placeholder = "Nombre de Usuario";
+	_apellido.placeholder = "Apellido de Usuario";
+	_dni.placeholder = "DNI de Usuario";
+	_nombre.readOnly = false;
+	_apellido.readOnly = false;
+	_dni.readOnly = false;
+
+	var num = asientos.indexOf(asiento);
+	celdas[num].style.backgroundColor = "White";
 	document.getElementById("mostrar").innerHTML = "El asiento " + asiento + " esta desocupado";
+	
 	var i=0;
-	for(i=0;i<usuarios.length;i++){
-		var j= usuarios[i].n;
-		if(usuarios[i].n==asiento){
+	for( i ; i < usuarios.length ; i++ ){
+		if(usuarios[i].n == asiento){
 			usuarios.splice(i,1);
 		}
 	}
-	
 }
 
 function buscar(){
 	var id = document.getElementById("DNI").value;
-	var mensaje="<h3><strong>Usuario No encontrado</strong></h3>"
-	for(var i=0; i<usuarios.length; i++){
-		if(usuarios[i].dni==id){
-			mensaje="<h3><strong>Usuario Encontrado:</strong></h3>"+
-					"<strong>Nombre de Usuario: </strong>"+ usuarios[i].nombre +"<br>"+ 
-					"<strong>Apellido de Usuario: </strong>"+ usuarios[i].apellido+"<br>"+
-					"<strong>DNI de Usuario: </strong>"+ usuarios[i].dni+"<br>"+
-					"<strong>Asiento Ocupado: </strong>"+ usuarios[i].n+"<br>"
+	var mensaje = "<h3><strong>Usuario No encontrado</strong></h3>"
+	for( var i = 0 ; i < usuarios.length ; i++ ){
+		if(usuarios[i].dni == id){
+			mensaje = "<h3><strong>Usuario Encontrado:</strong></h3>"+
+						"<strong>Nombre de Usuario: </strong>"+ usuarios[i].nombre +"<br>"+ 
+						"<strong>Apellido de Usuario: </strong>"+ usuarios[i].apellido+"<br>"+
+						"<strong>DNI de Usuario: </strong>"+ usuarios[i].dni+"<br>"+
+						"<strong>Asiento Ocupado: </strong>"+ usuarios[i].n+"<br>"
 		}  else {
-			mensaje="<h3><strong>Usuario No encontrado</strong></h3>"
+			mensaje = "<h3><strong>Usuario No encontrado</strong></h3>"
 		}	
 	}
-	document.getElementById("lista").innerHTML=mensaje;
-	document.getElementById("DNI").value="";
+	document.getElementById("lista").innerHTML = mensaje;
+	document.getElementById("DNI").value = "";
 }
 
 function limpiar(){
-
-   document.getElementById("nombre").value="";
-   document.getElementById("apellido").value="";
-   document.getElementById("dni").value="";
-   document.getElementById("lista").innerHTML="";
+   _nombre.value = "";
+   _apellido.value = "";
+   _dni.value = "";
+   document.getElementById("lista").innerHTML = "";
 }
